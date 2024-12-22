784d963e39d89063af16b95eb8fd83069aaa338d
Rachel Pastreich <rachpast@bu.edu>
2024-12-22 12:05:00 -0500

    added dependencies and assets

---
+++ react-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import styles from "./App.module.css";
+
+function App() {
+// use {} for variables to pass in as a classname
+  return (
+    <div className={styles.App}>
+      Hello World
+    </div>
+  )
+}
+
+export default App
