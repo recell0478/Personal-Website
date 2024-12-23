04fd442fb61b303c2922843d8b7a46037939eaed
Rachel Pastreich <rachpast@bu.edu>
2024-12-23 11:07:15 -0500

    commited

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,10 +1,11 @@
 import styles from "./App.module.css";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
 // use {} for variables to pass in as a classname
   return (
     <div className={styles.App}>
-      Hello World
+      <Navbar />
     </div>
   )
 }
--- react-portfolio/src/utils.js
+++ react-portfolio/src/utils.js
@@ -1,6 +1,3 @@
-// utility functions
 export const getImageUrl = (path) => {
-    // construct the absolute path
-    return new URL(`assets/${path}`, import.meta.url).href;
-    // taking a relative path and base url  - make it easier to import images
-};
+    return new URL(`/assets/${path}`, import.meta.url).href;
+  };
