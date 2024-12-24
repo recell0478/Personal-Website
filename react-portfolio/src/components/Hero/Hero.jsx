e1db403c39e1e214e5895a61837f1d556ed0be93
Rachel Pastreich <rachpast@bu.edu>
2024-12-24 11:34:50 -0500

    changed color

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -8,7 +8,6 @@ export const Hero = () => {
         {/* contains all the left hand side text */}
         <h1 className={styles.description}>Hello, I am Rachel Pastreich</h1>
         <p>My passion lies in full-stack software development, where I enjoy creating impactful solutions and continuously exploring new technologies. I also find great fulfillment in teaching and sharing knowledge with others.</p>
-        <p>   </p>
         <p>Outside of my academics, I am a course assistant for Data Structures and Algorithms course at BU, a member of Upsilon Pi Epsilon and event planner for Hack4Impact! I also enjoy hitting the gym, hunting for the best Pad Thai at Thai resturants, and traveling around the world!</p>
     <a href="rachpast@bu.edu" className={styles.contactBtn}>Contact Me</a>
     </div>
