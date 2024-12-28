b4c31350e5a6d0e30e1e815b6569ead738a088b8
Rachel Pastreich <rachpast@bu.edu>
2024-12-28 18:20:01 -0500

    changed image path

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -10,21 +10,21 @@ export const About = () => {
         <div className={styles.content}>
              <ul className={styles.aboutItems}>
                 <li className={styles.aboutItem}>
-                    <img src={getImageUrl("../../assets/about/cursorIcon.png")} alt="cursor icon" />
+                    <img src={getImageUrl("../assets/about/cursorIcon.png")} alt="cursor icon" />
                 <div className={styles.aboutItemText}> 
                     <h3>Frontend Developer</h3>
                     <p>I'm a frontend developer with experience in building responsive and optimized sites.</p>
                 </div>
                 </li>
                 <li className={styles.aboutItem}>
-                    <img src={getImageUrl("../../assets/about/serverIcon.png")} alt="server icon" />
+                    <img src={getImageUrl("../assets/about/serverIcon.png")} alt="server icon" />
                 <div className={styles.aboutItemText}>
                     <h3>Backend Developer</h3>
                     <p>I'm a backend developer with experience in developing fast and optimised back-end systems and APIs.</p>
                 </div>
                 </li>
                 <li className={styles.aboutItem}>
-                    <img src={getImageUrl("../../assets/about/uiIcon.png")} alt="UI icon" />
+                    <img src={getImageUrl("../assets/about/uiIcon.png")} alt="UI icon" />
                 <div className={styles.aboutItemText}>
                     <h3>Languages</h3>
                     <p>Java, Python, Javascript, Typescript, SQL, HTML, CSS, C, XML</p>
