732f135b64c10a3540bee2eddb87cb729ea86242
Rachel Pastreich <rachpast@bu.edu>
2024-12-28 19:16:38 -0500

    please last time

--- react-portfolio/src/components/Project/ProjectCard.jsx
+++ react-portfolio/src/components/Project/ProjectCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import {getImageUrl} from "../../utils";
 import styles from "./ProjectCard.module.css";
 
 export const ProjectCard = ({project : {title, imageSrc, description, skills, demo, source}}) => {
