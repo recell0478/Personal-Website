cf6a7a1cfe3c1f59d970ee690ad2728428079f5e
Rachel Pastreich <rachpast@bu.edu>
2024-12-30 14:01:23 -0500

    deleted json files

--- react-portfolio/src/components/Experience/Experience.jsx
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import skills from "../../data/skills.json";
-import history from "../../data/history.json";
 import styles from "./Experience.module.css";
-
+import reactpic from  "../../assets/skills/react.png";
+import node from "../../assets/skills/node.png";
+import express from "../../assets/skills/express.png";
+import mysql from "../../assets/skills/mysql.png";
+import css from "../../assets/skills/tailwind.png";
+import mongodb from "../../assets/skills/mongodb.png";
+import fidelity from "../../assets/history/fidelity.jpg";
+import bu from "../../assets/history/bu.png";
+import tutor from "../../assets/history/tutor.png";
 
 export const Experience = () => {
   return (
@@ -10,42 +16,91 @@ export const Experience = () => {
         <h2  className= {styles.title}>Experience</h2>
         <div  className= {styles.content}>
             <div  className= {styles.skills}>
-            {
-                skills.map((skill, id) => {
-                    return <div key={id}  className= {styles.skill}>
-                        <div  className= {styles.skillImageContainer}><img src={skill.imageSrc}
-                        alt={skill.title} />
-                        </div>
-                        <p>{skill.title}</p>
-                        </div>
+                <div className={styles.skill}>
+                    <div  className= {styles.skillImageContainer}><img src={reactpic}
+                            alt="React" />
+                    </div>
+                    <p>React</p>
+                </div>
 
+                <div className={styles.skill}>
+                    <div  className= {styles.skillImageContainer}><img src= {node}
+                            alt="Node" />
+                    </div>
+                    <p>Node</p>
+                </div>
 
-                })
-            }</div>
+                <div className={styles.skill}>
+                    <div  className= {styles.skillImageContainer}><img src={express}
+                            alt="Express" />
+                    </div>
+                    <p>Express</p>
+                </div>
+
+                <div className={styles.skill}>
+                    <div  className= {styles.skillImageContainer}><img src={mysql}
+                            alt="Mysql" />
+                    </div>
+                    <p>MySQL</p>
+                </div>
+
+                <div className={styles.skill}>
+                    <div  className= {styles.skillImageContainer}><img src={css
+
+                    }
+                            alt="Tailwind CSS" />
+                    </div>
+                    <p>Tailwind CSS</p>
+                </div>
+
+                <div className={styles.skill}>
+                    <div  className= {styles.skillImageContainer}><img src={mongodb}
+                            alt="MongoDB" />
+                    </div>
+                    <p>MongoDB</p>
+                </div>
+            </div>
+
             <ul  className= {styles.history}>
-                {history.map((historyItem, id) => {
-                        return (<li key={id}  className= {styles.historyItem}>
-                            <img 
-                            src= {historyItem.imageSrc}
-                            alt={`${historyItem.organisation} Logo`} />
-                            <div  className= {styles.historyItemDetails}>
-                                <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
-                                <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                <li className={styles.historyItem}>
+                <img src= {fidelity}
+                            alt={`Fidelity Logo`} />
+                             <div  className= {styles.historyItemDetails}>
+                                <h3>Full-Stack Software Engineer, Fidelity</h3>
+                                <p>June, 2025 - Present</p>
                                 <ul>
-                                    {historyItem.experiences.map((experience, id) => {
-                                        return (
-                                            <li key={id}>
-                                                {experience}
-                                            </li>
-                                        )
-                                        
-                                    })}
+                                <li>Incoming Full-Stack Software engineer at Fidelity for summer 2025</li>
                                 </ul>
-                            </div>
-                        </li>
-                        );
-                    })
-                }
+                                </div>
+                </li>
+
+                <li className={styles.historyItem}>
+                <img src= {bu}
+                            alt={`BU Logo`} />
+                             <div  className= {styles.historyItemDetails}>
+                                <h3>Course Assistant, Boston University</h3>
+                                <p>Sept 2024 - Present</p>
+                                <ul>
+
+            
+                                <li>Hosted office hours for 250+ enrolled students</li>
+                                <li>Facilitated lab sections, guiding 20+ students through programming exercises</li>
+                                </ul>
+                                </div>
+                </li>
+
+                <li className={styles.historyItem}>
+                <img src= {tutor}
+                            alt={`Club Z! Tutoring Logo`} />
+                             <div  className= {styles.historyItemDetails}>
+                                <h3>Math Tutor, Club Z! Tutoringy</h3>
+                                <p>Nov 2023 - Present</p>
+                                <ul>
+                                <li>Tutored two high school students in Calculus and Linear Algebra 4 hours per week</li>
+                                <li>Designed tailored lesson plans to individual learning styles and needs</li>
+                                </ul>
+                                </div>
+                </li>
             </ul>
         </div>
         <div className={styles.topBlur} />
--- react-portfolio/src/components/Project/Project.jsx
+++ react-portfolio/src/components/Project/Project.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
-import projects from "../../data/project.json";
-import { ProjectCard } from './ProjectCard';
 import styles from "./Project.module.css";
+import project1 from "../../assets/projects/gameoflife.png";
+import project2 from "../../assets/projects/upe.png";
 
 export const Project = () => {
   return (
     <section className={styles.container} id="projects">
         <h2 className={styles.title}>Projects</h2>
         <div className={styles.projects}> 
-    {
-        projects.map((project, id) => {
-            return (
-                <ProjectCard key={id} project={project} />
-            )
-        })
-    }
+            <div className={styles.project}>
+                <h3 className={styles.subtitle}>Game of Life</h3>
+                <img className = {styles.image} src= {project1} alt={`Image of Game of life`}/>
+                <p className={styles.description}>Collaborated in a team of four to develop an interactive financial literacy quiz platform using React, TypeScript, and Tailwind CSS, featuring a point system to encourage personal finance improvement, a resource library on budgeting, saving, and investing, and seamless navigation with React Router</p>
+                <ul className={styles.skills} >
+                    <li className={styles.skill}>React</li>
+                    <li className={styles.skill}>Typescript</li>
+                    <li className={styles.skill}>Tailwind CSS</li>
+                    <li className={styles.skill}>HTML</li>
+                    
+                </ul>
+                <div className={styles.links}>
+                         <a href={"https://gameof-life-9dec.vercel.app/"} className={styles.link}>Demo</a>
+                      <a href={"https://github.com/recell0478/GameofLife"} className={styles.link}>Source</a>
+    
+                     </div>
+                </div>
+                <div className={styles.project}>
+                <h3 className={styles.subtitle}>Upsilon Pi Eplsilon Website</h3>
+                <img className = {styles.image} src= {project2} alt={`Image of Upsilon Pi Eplsilon Website`}/>
+                <p className={styles.description}>Designed and developed the Boston University Upsilon Pi Epsilon chapter website, serving 50+ active users, by utilizing React.js, HTML, CSS, and JavaScript for the front-end and Express.js and MongoDB for the back-end to optimize user experience and ensure cross-device functionality</p>
+                <ul className={styles.skills} >
+                    <li className={styles.skill}>React</li>
+                    <li className={styles.skill}>Express</li>
+                    <li className={styles.skill}>HTML</li>
+                    <li className={styles.skill}>CSS</li>
+                    <li className={styles.skill}>MongoDB</li>
+                
+                </ul>
+                <div className={styles.links}>
+                         <a href={"https://github.com/recell0478/upe-website"} className={styles.link}>Demo</a>
+                      <a href={"https://github.com/recell0478/upe-website"} className={styles.link}>Source</a>
+    
+                     </div>
+                </div>
         </div>
-         <div className={styles.topBlur} />
-         <div className={styles.bottomBlur} />
     </section>
   )
 }
